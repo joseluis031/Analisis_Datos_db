58bc2c7c37649877c8624825caade556eabfd445
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2024-02-05 15:08:53 +0100

    diagrama

--- Tratmiento_DATOS/analisis.py
+++ Tratmiento_DATOS/analisis.py
@@ -44,11 +44,35 @@ def regresion_lineal(datos, columna_puntajes, columna_categorica_numerica):
     plt.xlabel('Puntaje')
     plt.ylabel(columna_categorica_numerica.capitalize())
     plt.show()
+    
+def diagrama_sectores_paises(datos, columna_categorica, umbral_paises=8):
+    plt.figure(figsize=(10, 6))
+
+    # Obtener la frecuencia de equipos por país
+    frecuencia_paises = datos[columna_categorica].value_counts()
+
+    # Identificar los últimos 8 países con menor frecuencia
+    paises_resto = frecuencia_paises.nsmallest(umbral_paises)
+
+    # Agrupar países con menos del umbral en "Resto de Europa"
+    paises_resto_total = pd.Series({'Resto de Europa': paises_resto.sum()})
+
+    # Excluir los últimos 8 países de la categoría "Resto de Países"
+    paises_principales = frecuencia_paises.drop(paises_resto.index)
+    frecuencia_final = paises_principales.append(paises_resto_total)
+
+    # Crear el diagrama de sectores
+    frecuencia_final.plot.pie(autopct='%1.1f%%', startangle=90, cmap='viridis')
+    plt.title(f'Cantidad de Equipos por {columna_categorica.capitalize()} en la competición')
+    plt.ylabel('')
+    plt.show()
 
 ruta_csv = 'CSVS/equipos_modificado.csv'
 datos = cargar_datos(ruta_csv)
+
 resumen_estadistico(datos)
 calcular_media_puntajes(datos, 'puntaje')
 visualizar_distribucion_puntajes(datos, 'puntaje')
 grafico_barras_promedio(datos, 'pais', 'puntaje')
 regresion_lineal(datos, 'puntaje', 'pais_numerico')
+diagrama_sectores_paises(datos, 'pais')
