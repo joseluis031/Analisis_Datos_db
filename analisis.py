c70831b616bf8f97b01efe44eeae4251a12648e5
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2024-02-12 16:09:44 +0100

    cambios csv y analisis

--- analisis.py
+++ analisis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+
 # Cargar los datos de los archivos CSV
 resultados_actual = pd.read_csv('Eliminatoria actual/eliminatoria.csv')
 
@@ -21,13 +22,14 @@ equipos_octavos_visitante = resultados_actual[resultados_actual['fase'] == 'Octa
 equipos_octavos = list(set(equipos_octavos_local + equipos_octavos_visitante))
 
 # Limpiar nombres de equipos en el conjunto de datos anterior
-resultados_anterior['equipo_local'] = resultados_anterior['equipo_local'].apply(lambda x: x.strip() if isinstance(x, str) else x)
-resultados_anterior['equipo_visitante'] = resultados_anterior['equipo_visitante'].apply(lambda x: x.strip() if isinstance(x, str) else x)
+resultados_anterior['equipo_local'] = resultados_anterior['equipo_local'].str.strip()
+resultados_anterior['equipo_visitante'] = resultados_anterior['equipo_visitante'].str.strip()
 
 # Filtrar los datos de la temporada anterior para incluir solo equipos en octavos
-resultados_anterior_octavos_local = resultados_anterior[resultados_anterior['equipo_local'].isin(equipos_octavos)]
-resultados_anterior_octavos_visitante = resultados_anterior[resultados_anterior['equipo_visitante'].isin(equipos_octavos)]
-resultados_anterior_octavos = pd.concat([resultados_anterior_octavos_local, resultados_anterior_octavos_visitante])
+resultados_anterior_octavos = resultados_anterior[
+    (resultados_anterior['equipo_local'].isin(equipos_octavos)) |
+    (resultados_anterior['equipo_visitante'].isin(equipos_octavos))
+]
 
 # Calcular goles marcados y recibidos para cada equipo en la temporada anterior
 goles_marcados_local = {}
@@ -36,11 +38,29 @@ goles_recibidos_local = {}
 goles_recibidos_visitante = {}
 
 for equipo in equipos_octavos:
-    goles_marcados_local[equipo] = resultados_anterior_octavos[resultados_anterior_octavos['equipo_local'] == equipo]['goles_equipo_local'].sum()
-    goles_marcados_visitante[equipo] = resultados_anterior_octavos[resultados_anterior_octavos['equipo_visitante'] == equipo]['goles_equipo_visitante'].sum()
+    # Goles marcados como local
+    goles_marcados_local[equipo] = resultados_anterior_octavos.loc[
+        (resultados_anterior_octavos['equipo_local'] == equipo),
+        'goles_equipo_local'
+    ].sum()
+
+    # Goles marcados como visitante
+    goles_marcados_visitante[equipo] = resultados_anterior_octavos.loc[
+        (resultados_anterior_octavos['equipo_visitante'] == equipo),
+        'goles_equipo_visitante'
+    ].sum()
     
-    goles_recibidos_local[equipo] = resultados_anterior_octavos[resultados_anterior_octavos['equipo_local'] == equipo]['goles_equipo_visitante'].sum()
-    goles_recibidos_visitante[equipo] = resultados_anterior_octavos[resultados_anterior_octavos['equipo_visitante'] == equipo]['goles_equipo_local'].sum()
+    # Goles recibidos como local
+    goles_recibidos_local[equipo] = resultados_anterior_octavos.loc[
+        (resultados_anterior_octavos['equipo_local'] == equipo),
+        'goles_equipo_visitante'
+    ].sum()
+
+    # Goles recibidos como visitante
+    goles_recibidos_visitante[equipo] = resultados_anterior_octavos.loc[
+        (resultados_anterior_octavos['equipo_visitante'] == equipo),
+        'goles_equipo_local'
+    ].sum()
 
 # Crear un DataFrame con los resultados
 tabla_goles = pd.DataFrame({
