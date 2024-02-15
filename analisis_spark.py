fc27d2b78060d36e68c0b4910e8a7565b3605a5f
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2024-02-15 11:10:40 +0100

    empiezo pyspark

---
+++ analisis_spark.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+
+# Crea una sesión de Spark
+spark = SparkSession.builder.appName("ejemplo_csv").getOrCreate()
+
+# Ruta al archivo CSV
+archivo_csv = "ruta/al/tu/archivo.csv"
+
+# Lee el archivo CSV en un DataFrame de Spark
+dataframe = spark.read.csv(archivo_csv, header=True, inferSchema=True)
+
+# Muestra el contenido del DataFrame
+dataframe.show()
+
+# Puedes realizar operaciones adicionales en el DataFrame según tus necesidades
+# Por ejemplo, puedes filtrar, agrupar, realizar transformaciones, etc.
+
+# Cierra la sesión de Spark al finalizar
+spark.stop()
